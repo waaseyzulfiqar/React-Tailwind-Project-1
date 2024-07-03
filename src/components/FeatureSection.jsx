88df717754bed35fa790b22a62fd8225e0aef27b
waaseyzulfiqar <wasey03112920792@gmail.com>
2024-07-03 18:29:29 +0500

    feature page completed

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -17,7 +17,23 @@ export const FeatureSection = () => {
         </h2>
       </div>
 
-      <div className="flex flex-wrap mt-10 lg:mt-20"></div>
+      <div className="flex flex-wrap mt-10 lg:mt-20">
+        {features.map((feature,index) => (
+          <div key={index} className="w-full sm:1/2 lg:w-1/3">
+            <div className="flex">
+              <div className="flex mx-6 h-10 w-10 p-2 bg-neutral-900 text-orange-700 justify-center items-center rounded-full">
+                {feature.icon}
+              </div>
+
+              <div>
+                <h5 className="mt-1 mb-6 text-lg">{feature.text}</h5>
+                <p className="text-base p-2 mb-20 text-neutral-500">{feature.description}</p>
+              </div>
+
+            </div>
+          </div>
+        ))}
+      </div>
 
     </div>
   );
