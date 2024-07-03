e8c5bb5d3719fb0ede73fd362ea85651a48fa01a
waaseyzulfiqar <wasey03112920792@gmail.com>
2024-07-03 20:43:49 +0500

    footer completed

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -36,13 +36,13 @@ export const testimonials = [
     user: "David Johnson",
     company: "Quantum Innovations",
     image: user3,
-    text: "Working with this company was a pleasure. Their attention to detail and commitment to excellence are commendable. I would highly recommend them to anyone looking for top-notch service.",
+    text: "Working with this company was a pleasure. Their attention to detail and commitment to excellence are commendable. I would highly recommend them to anyone",
   },
   {
     user: "Ronee Brown",
     company: "Fusion Dynamics",
     image: user4,
-    text: "Working with the team at XYZ Company was a game-changer for our project. Their attention to detail and innovative solutions helped us achieve our goals faster than we thought possible. We are grateful for their expertise and professionalism!",
+    text: "Working with the team at XYZ Company was a game-changer for our project. Their attention to detail and innovative solutions helped us achieve our goals faster",
   },
   {
     user: "Michael Wilson",
@@ -54,7 +54,7 @@ export const testimonials = [
     user: "Emily Davis",
     company: "Synergy Systems",
     image: user6,
-    text: "The team went above and beyond to ensure our project was a success. Their expertise and dedication are unmatched. I look forward to working with them again in the future.",
+    text: "The team went above and beyond to ensure our project was a success. Their expertise and dedication are unmatched. I look forward to  ",
   },
 ];
 
