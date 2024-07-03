363ad703af4886ae18558f0a10f3e42e692f8253
waaseyzulfiqar <wasey03112920792@gmail.com>
2024-07-03 17:03:08 +0500

    hero section completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { HeroSection } from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <div className="max-w-6xl mx-auto pt-20 px-6">
+
+      <HeroSection />
+
+      </div>
     </>
   );
 }
